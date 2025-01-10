3fd2c293eb13758e0abc0f335aa7efe09d5632ac
Siddique Naeem <sddqnaeem@gmail.com>
2025-01-10 23:51:36 +0600

    intialize the project boilerplate

---
+++ SemanticSimilarity/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SemanticSimilarity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Semantic Similarity Analysis - Initial Setup");
+        }
+    }
+}
