689cc57725bc16f86a274ae81ab43a441097b145
Siddique Naeem <sddqnaeem@gmail.com>
2025-01-11 01:35:37 +0600

    test openai nuget package with .env file

--- SemanticSimilarity/SemanticSimilarity/Program.cs
+++ SemanticSimilarity/SemanticSimilarity/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace SemanticSimilarity
+﻿using DotNetEnv;
+using OpenAI.Chat;
+
+namespace SemanticSimilarity
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Semantic Similarity Analysis - Initial Setup");
+            Env.Load();
+
+            ChatClient client = new(model: "gpt-4o", apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+
+            ChatCompletion completion = client.CompleteChat("Say 'this is a test.'");
+
+            Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
         }
     }
 }
