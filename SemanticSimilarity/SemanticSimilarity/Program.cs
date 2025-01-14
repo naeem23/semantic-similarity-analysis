9bf0997caf5fdd9d765508b2a6803b8538bf37e3
Siddique Naeem <sddqnaeem@gmail.com>
2025-01-14 20:57:16 +0600

    Using the OpenAIClient class

--- SemanticSimilarity/SemanticSimilarity/Program.cs
+++ SemanticSimilarity/SemanticSimilarity/Program.cs
@@ -1,19 +1,31 @@
 ﻿using DotNetEnv;
+using OpenAI;
+using OpenAI.Audio;
 using OpenAI.Chat;
 
 namespace SemanticSimilarity
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Env.Load();
 
-            ChatClient client = new(model: "gpt-4o", apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+            //ChatClient client = new(model: "gpt-4o", apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
 
-            ChatCompletion completion = client.CompleteChat("Say 'this is a test.'");
+            //ChatCompletion completion = client.CompleteChat("Say 'this is a test.'");
 
-            Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+            //Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+            // Input texts for embeddings
+
+            OpenAIClient client = new(Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+
+            AudioClient ttsClient = client.GetAudioClient("tts-1");
+            AudioClient whisperClient = client.GetAudioClient("whisper-1");
+
+            Console.WriteLine($"tts client {ttsClient}");
+            Console.WriteLine($"whisper client {whisperClient}");
         }
+
     }
 }
