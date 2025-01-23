43891c56d291feedb4180a15cbffb5c8299cba30
MD. ABDUL AHAD <43789552+mdabdulahadbappy@users.noreply.github.com>
2025-01-23 04:45:29 +0100

    Update Program.cs
    Main Program Update

--- SemanticSimilarity/SemanticSimilarity/Program.cs
+++ SemanticSimilarity/SemanticSimilarity/Program.cs
@@ -1,5 +1,8 @@
-﻿//using DotNetEnv;
-//using OpenAI.Chat;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SemanticSimilarity
 {
