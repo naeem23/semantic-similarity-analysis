b15974b76900b2d37c452d7dfd4de3da877eb6e9
MD. ABDUL AHAD <43789552+mdabdulahadbappy@users.noreply.github.com>
2025-01-26 18:38:19 +0100

    DocumentProcessor

--- SemanticSimilarity/SemanticSimilarity/DocumentProcessor.cs
+++ SemanticSimilarity/SemanticSimilarity/DocumentProcessor.cs
@@ -5,3 +5,12 @@ using System.Linq;
 using Newtonsoft.Json;
 using RestSharp;
 using System.Threading.Tasks;
+public class DocumentProcessor
+{
+    private readonly string _apiKey;
+    private const string BaseUrl = "https://api.openai.com/v1/embeddings";
+
+    public DocumentProcessor(string apiKey)
+    {
+        _apiKey = apiKey;
+    }
