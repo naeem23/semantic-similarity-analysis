8f336693c3b0f7d779e3049f30dffc736df5c483
Haimanti <160922357+Haimanti97@users.noreply.github.com>
2025-01-27 23:21:29 +0100

    Update OpenAIClient class

--- SemanticSimilarity/SemanticSimilarity/OpenAIClient.cs
+++ SemanticSimilarity/SemanticSimilarity/OpenAIClient.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SemanticSimilarity
+public class OpenAIClient
 {
-    internal class OpenAIClient
+    private readonly string _apiKey;
+    private const string BaseUrl = "https://api.openai.com/v1";
+
+    public OpenAIClient(string apiKey)
     {
+        _apiKey = apiKey;
     }
-}
+
+    public async Task<string> GetEmbedding(string text, string model = "text-embedding-ada-002")
+    {
+        var client = new RestClient($"{BaseUrl}/embeddings");
+        //var request = new RestRequest(Method.Post); // Updated Syntax
+        var request = new RestRequest();
+        request.Method = Method.Post;
