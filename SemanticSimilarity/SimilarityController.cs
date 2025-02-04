5af32db104d5c6169a11a0c2907dc17d0b6e37fa
Haimanti <160922357+Haimanti97@users.noreply.github.com>
2025-02-04 10:59:59 +0100

    Update SimilarityController.cs

--- SemanticSimilarity/SimilarityController.cs
+++ SemanticSimilarity/SimilarityController.cs
@@ -8,3 +8,8 @@ namespace SemanticSimilarityAPI.Controllers
     [Route("api/similarity")]
     [ApiController]
     public class SimilarityController : ControllerBase
+    {
+        private static readonly MLContext mlContext = new MLContext();
+
+        [HttpPost]
+        public IActionResult ComputeSimilarity([FromBody] SimilarityRequest request)
