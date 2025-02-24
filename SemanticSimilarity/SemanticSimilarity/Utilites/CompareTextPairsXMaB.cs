fb965589565e8d6844ed76be6a4bc611794c465d
mdabdulahadbappy <43789552+mdabdulahadbappy@users.noreply.github.com>
2025-02-24 11:10:34 +0100

    CompareTextPairsXMaB

--- SemanticSimilarity/SemanticSimilarity/Utilites/CompareTextPairsXMaB.cs
+++ SemanticSimilarity/SemanticSimilarity/Utilites/CompareTextPairsXMaB.cs
@@ -8,5 +8,13 @@ namespace SemanticSimilarity.Utilites
 {
     internal class CompareTextPairsXMaB
     {
-    }
+
+        static async Task CompareTextPairs(DocumentProcessor processor, List<string> texts, string category, List<(string, string, double)> results)
+        {
+            Console.WriteLine($"\n==== {category} Semantic Similarity ====");
+
+            for (int i = 0; i < texts.Count; i++)
+            {
+
+            }
 }
