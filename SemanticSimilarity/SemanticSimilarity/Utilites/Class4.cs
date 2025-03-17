143c700764f8aa4e3fc8cdbdd5265ac5fa776a7d
Haimanti <biswashaimanti33@gmail.com>
2025-03-17 16:50:35 +0100

    Heatmap Visualization for Similarity Matrix

---
+++ SemanticSimilarity/SemanticSimilarity/Utilites/Class4.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+public partial class MainWindow : Window
+{
+    public ObservableCollection<ObservableCollection<double>> SimilarityMatrix { get; set; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        SimilarityMatrix = ComputeSimilarityMatrix();
+        DataContext = this;
+    }
+
+    private ObservableCollection<ObservableCollection<double>> ComputeSimilarityMatrix()
+    {
+        List<string> texts = new List<string> { "AI is smart", "Machine learning is AI", "AI is powerful" };
+        var matrix = new ObservableCollection<ObservableCollection<double>>();
+
+        foreach (var text1 in texts)
+        {
+            var row = new ObservableCollection<double>();
+            foreach (var text2 in texts)
+            {
+                row.Add(TFIDFHelper.ComputeTFIDFSimilarity(text1, text2, texts));
+            }
+            matrix.Add(row);
+        }
+
+        return matrix;
+    }
+}
