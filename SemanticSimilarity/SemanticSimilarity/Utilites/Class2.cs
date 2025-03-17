2643b2a8920a057b2d924a0d3b0f14f6027c0436
Haimanti <biswashaimanti33@gmail.com>
2025-03-17 16:40:34 +0100

    Euclidean Distance for Text Similarity

---
+++ SemanticSimilarity/SemanticSimilarity/Utilites/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class EuclideanSimilarity
+{
+    public static double ComputeEuclideanDistance(List<double> vector1, List<double> vector2)
+    {
+        if (vector1.Count != vector2.Count)
+            throw new ArgumentException("Vectors must be of the same length");
+
+        double sum = vector1.Zip(vector2, (v1, v2) => Math.Pow(v1 - v2, 2)).Sum();
+        return Math.Sqrt(sum);
+    }
+}
