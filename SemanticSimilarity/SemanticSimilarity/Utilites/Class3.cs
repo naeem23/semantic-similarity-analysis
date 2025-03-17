d37b8df9b95961caf3154232c9e3cd2cf704856c
Haimanti <biswashaimanti33@gmail.com>
2025-03-17 16:48:38 +0100

    Real-Time Line Graph Updates

---
+++ SemanticSimilarity/SemanticSimilarity/Utilites/Class3.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+public partial class MainWindow : Window
+{
+    public SeriesCollection SeriesCollection { get; set; }
+    public ObservableCollection<double> SimilarityScores { get; set; } = new ObservableCollection<double>();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        SeriesCollection = new SeriesCollection
+        {
+            new LineSeries
+            {
+                Title = "Similarity Score",
+                Values = new ChartValues<double>(SimilarityScores)
+            }
+        };
+        DataContext = this;
+    }
+
+    private void UpdateGraph(double newScore)
+    {
+        SimilarityScores.Add(newScore);
+    }
+}
